bae92a424a546d1c2317afc5afcfeebbfda621dc
Isay <Razilov>
2019-03-18 17:38:53 +0200

    section 11 done. working app.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube' 
 import VideoList from './VideoList' 
+import VideoDetial from './VideoDetial' 
 
 class App extends React.Component {
 
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('cats')
+    }
     onTermSubmit = async term => {
 
         // the api request 
@@ -15,14 +19,30 @@ class App extends React.Component {
                 q: term
             }
         });
-        this.setState({ videos: response.data.items });
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
+
+    onVideoSelect = video => {
+        this.setState({selectedVideo: video})
+    }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetial video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
